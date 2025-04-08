aed2c8457799defd50e8d5c0d883e6b4c6865ed0
elianortega <elian.mortega@gmail.com>
2025-04-08 10:08:46 -0600

    feat: change main language to spanish

--- shared/parameters.js
+++ shared/parameters.js
@@ -1,1 +1,2 @@
-const language = en; //avaliable languages: english (en), hungarian (hu), french (fr), spanish (es), portuguese (pt)
+const language = es; //avaliable languages: english (en), hungarian (hu), french (fr), spanish (es), portuguese (pt)
+
